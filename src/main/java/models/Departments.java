ad9eac681a37e79a2ea3aaf610cf1d7b2acbd9f6
Belyse Uwambayinema <ubelyse1@gmail.com>
2020-12-06 21:35:16 +0200

    tested the department

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -1,5 +1,67 @@
 package models;
 
+import java.util.Objects;
+
 public class Departments {
 
+    private String dept_name;
+    private String dept_description;
+    private int dept_empNo;
+    private int id;
+
+    //Constructor
+    public Departments(String dept_name, String dept_description, int dept_empNo) {
+        this.dept_name = dept_name;
+        this.dept_description = dept_description;
+        this.dept_empNo = dept_empNo;
+
+    }
+    //Override
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Departments that = (Departments) o;
+        return dept_empNo == that.dept_empNo &&
+                Objects.equals(dept_name, that.dept_name) &&
+                Objects.equals(dept_description, that.dept_description) &&
+                Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dept_name, dept_description, dept_empNo, id);
+    }
+
+    public String getDpt_name() {
+        return dept_name;
+    }
+
+    public void setDpt_name(String dept_name) {
+        this.dept_name = dept_name;
+    }
+
+    public String getDpt_description() {
+        return dept_description;
+    }
+
+    public void setDpt_description(String dept_description) {
+        this.dept_description = dept_description;
+    }
+
+    public int getDpt_empNo() {
+        return dept_empNo;
+    }
+
+    public void setDpt_empNo(int dept_empNo) {
+        this.dept_empNo = dept_empNo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/test/java/models/DepartmentsTest.java
+++ src/test/java/models/DepartmentsTest.java
@@ -1,7 +1,61 @@
 package models;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DepartmentsTest {
 
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    //get DepartmentName
+    @Test
+    public void getDepartmentName() throws Exception {
+        Departments departments = setDepartments();
+        assertEquals("hr", departments.getDpt_name());
+    }
+
+    //get Department Description
+    @Test
+    public void getDepartmentDescription() throws Exception {
+        Departments departments = setDepartments();
+        assertEquals("recruiting", departments.getDpt_description());
+    }
+
+    @Test
+    public void getEmployeesNumbers() throws Exception {
+        Departments departments = setDepartments();
+        assertEquals(20, departments.getDpt_empNo());
+    }
+    @Test
+    public void setEmployeesNumber(){
+        Departments departments = setDepartments();
+        departments.setDpt_empNo(20);
+        assertNotEquals(10, departments.getDpt_empNo());
+    }
+
+    @Test
+    public void setDepartmentName(){
+        Departments departments = setDepartments();
+        departments.setDpt_name("HR");
+        assertNotEquals("office", departments.getDpt_name());
+
+    }
+    @Test
+    public void setDepartmentsDescription(){
+        Departments departments = setDepartments();
+        departments.setDpt_description("recruiting");
+        assertNotEquals("driver", departments.getDpt_description());
+    }
+
+    public Departments setDepartments() {
+        return new Departments("hr", "recruiting", 20);
+    }
 }
