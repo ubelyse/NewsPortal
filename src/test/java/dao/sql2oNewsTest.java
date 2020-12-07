ade47c29b81e9b8b997e0620c8c93c2f1126ac68
Belyse Uwambayinema <ubelyse1@gmail.com>
2020-12-07 14:44:58 +0200

    edited news test

--- src/test/java/dao/sql2oNewsTest.java
+++ src/test/java/dao/sql2oNewsTest.java
@@ -1,8 +1,13 @@
 package dao;
 
 import static org.junit.Assert.*;
+
+import models.Department_news;
+import models.Departments;
 import models.News;
+import models.Users;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 import org.sql2o.Connection;
@@ -11,32 +16,102 @@ import org.sql2o.Sql2o;
 
 public class sql2oNewsTest {
 
+    private static sql2oDepartments sql2oDepartments;
+    private static sql2oUsers sql2oUsersDao;
+    private static sql2oNews sql2oNewsDao;
     private static Connection conn;
-    private static sql2oNews newsDao;
-    private static sql2oDepartments departmentsDao;
-
-    public News setupNews(){
-        return new News(1,"snitch","guess who's broke?");
-    }
 
     @Before
     public void setUp() throws Exception {
+
         String connectionString = "jdbc:postgresql://localhost:5432/newsportal_test";
         Sql2o sql2o = new Sql2o(connectionString, "belyse", "belyse");
-        newsDao = new sql2oNews(sql2o);
-        conn = sql2o.open();
+
+
+        sql2oDepartments=new sql2oDepartments(sql2o);
+        sql2oUsersDao=new sql2oUsers(sql2o);
+        sql2oNewsDao=new sql2oNews(sql2o);
+        System.out.println("connected");
+        conn=sql2o.open();
+
     }
 
     @After
     public void tearDown() throws Exception {
+        sql2oDepartments.clearAllDept();
+        sql2oUsersDao.clearAllUsers();
+        sql2oNewsDao.clearAllNews();
+        System.out.println("clearing");
+    }
+
+    @AfterClass
+    public static void shutDown() throws Exception{
         conn.close();
+        System.out.println("connection closed");
     }
-    //Add news
+
     @Test
-    public void addNews() throws Exception{
-        News testNews = setupNews();
-        newsDao.add(testNews);
-        int newId = testNews.getNews_id();
-        assertEquals(newId,testNews.getNews_id());
+    public void addNews() {
+        Users users=setUsers();
+        sql2oUsersDao.add(users);
+        Departments departments=setDepartment();
+        sql2oDepartments.addDept(departments);
+        News news=new News("payments","paying salary",users.getId());
+        sql2oNewsDao.addNews(news);
+
+        assertEquals(users.getId(),sql2oNewsDao.findById(news.getId()).getUid());
+        assertEquals(news.getDeptid(),sql2oNewsDao.findById(news.getId()).getDeptid());
     }
+
+
+
+
+    @Test
+    public void addDepartmentNews() {
+        Users users=setUsers();
+        sql2oUsersDao.add(users);
+        Departments departments=setDepartment();
+        sql2oDepartments.addDept(departments);
+        Department_news department_news =new Department_news("payments","paying salary",departments.getId()
+                ,users.getId());
+        sql2oNewsDao.addDepartmentNews(department_news);
+        assertEquals(users.getId(),sql2oNewsDao.findById(department_news.getId()).getUid());
+        assertEquals(department_news.getDeptid(),sql2oNewsDao.findById(department_news.getId()).getDeptid());
+
+    }
+
+
+
+
+    @Test
+    public void getAll() {
+        Users users=setUsers();
+        sql2oUsersDao.add(users);
+        Departments departments=setDepartment();
+        sql2oDepartments.addDept(departments);
+        Department_news department_news =new Department_news("payments","paying salary",departments.getId()
+                ,users.getId());
+        sql2oNewsDao.addDepartmentNews(department_news);
+        News news=new News("payments","paying salary",users.getId());
+        sql2oNewsDao.addNews(news);
+        assertEquals(2,sql2oNewsDao.getAll().size());
+    }
+
+
+
+    @Test
+    public void findById() {
+    }
+
+    //helper
+//    private News setUpNewNews() {
+//        return new News("Meeting","Meeting to set activities for team building");
+//    }
+    private Departments setDepartment() {
+        return new Departments("sale","marketing");
+    }
+    private Users setUsers() {
+        return new Users("belyse","hr","recruiting");
+    }
+
 }
--- src/test/java/dao/sql2oUsersTest.java
+++ src/test/java/dao/sql2oUsersTest.java
@@ -1,7 +1,97 @@
 package dao;
 
+import models.Departments;
+import models.Users;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
 import static org.junit.Assert.*;
 
 public class sql2oUsersTest {
 
+    private static sql2oDepartments sql2oDepartments;
+    private static sql2oUsers sql2oUsers;
+    private static Connection conn;
+
+    @Before
+    public void setUp() throws Exception {
+
+        //uncomment the two lines below to run locally and change to your  credentials
+        String connectionString = "jdbc:postgresql://localhost:5432/newsportal_test";
+        Sql2o sql2o = new Sql2o(connectionString, "belyse", "belyse");
+
+        sql2oDepartments=new sql2oDepartments(sql2o);
+        sql2oUsers=new sql2oUsers(sql2o);
+        System.out.println("connected");
+        conn=sql2o.open();
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        sql2oDepartments.clearAllDept();
+        sql2oUsers.clearAllUsers();
+        System.out.println("clearing");
+    }
+    @AfterClass
+    public static void shutDown() throws Exception{
+        conn.close();
+        System.out.println("connection closed");
+    }
+
+
+    @Test
+    public void addingUser() {
+        Users user = setNewUser();
+        int uid= user.getId();
+        sql2oUsers.add(user);
+        assertNotEquals(uid,user.getId());
+    }
+
+    @Test
+    public void addedUser() {
+        Users user = setNewUser();
+        sql2oUsers.add(user);
+        assertEquals(user.getName(),sql2oUsers.findById(user.getId()).getName());
+    }
+
+    @Test
+    public void UserInstances() {
+
+        Users users=setNewUser();
+        Users otherUser= new Users("belyse","hr","recruiting");
+        sql2oUsers.add(users);
+        sql2oUsers.add(otherUser);
+        assertEquals(users.getName(),sql2oUsers.getAll().get(0).getName());
+        assertEquals(otherUser.getName(),sql2oUsers.getAll().get(1).getName());
+    }
+    @Test
+    public void getDepartmentsUserIsIn() {
+        Departments department=setDepartment();
+        Departments otherDepartment=new Departments("sale","marketing");
+        sql2oDepartments.addDept(department);
+        sql2oDepartments.addDept(otherDepartment);
+        Users user=setNewUser();
+        Users otherUser= new Users("belyse","hr","recruiting");
+        sql2oUsers.add(user);
+        sql2oUsers.add(otherUser);
+        sql2oDepartments.addUserIntoDept(user,department);
+        sql2oDepartments.addUserIntoDept(otherUser,department);
+        sql2oDepartments.addUserIntoDept(user,otherDepartment);
+        assertEquals(2,sql2oUsers.getAllUserDepartments(user.getId()).size());
+        assertEquals(1,sql2oUsers.getAllUserDepartments(otherUser.getId()).size());
+    }
+
+    //helper
+    private Users setNewUser() {
+        return new Users("belyse","hr","recruiting");
+    }
+    private Departments setDepartment() {
+        return new Departments("hr","marketing");
+    }
 }
