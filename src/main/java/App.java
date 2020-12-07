9e9df37b64e248ead4053cadbbdfae335228b2d7
Belyse Uwambayinema <ubelyse1@gmail.com>
2020-12-07 09:53:55 +0200

    create new department

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,6 +17,27 @@ import static spark.Spark.*;
 public class App {
 
     public static void main(String[] args) {
+        sql2oDepartments departmentsDao;
+        sql2oNews newsDao;
+        sql2oEmployees employeesDao;
+        Connection conn;
+        Gson gson = new Gson();
 
+        String connectionString = "jdbc:postgresql://localhost:5432/newsportal";
+        Sql2o sql2o = new Sql2o(connectionString, "belyse", "belyse");
+
+        departmentsDao = new sql2oDepartments(sql2o);
+        employeesDao = new sql2oEmployees(sql2o);
+        newsDao = new sql2oNews(sql2o);
+        conn = sql2o.open();
+
+        //create
+        post("/departments/new", "application/json", (req, res) -> {
+            Departments departments = gson.fromJson(req.body(), Departments.class);
+            departmentsDao.add(departments);
+            res.status(201);
+            res.type("application/json");
+            return gson.toJson(departmentsDao.getAllDepartments());
+        });
     }
 }
